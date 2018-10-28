e4d0693ec3700bcb6c7fb65d823949ee3167ec3f
arnaldosouza <arnaldoeloi2@gmail.com>
2018-10-28 00:12:36 -0300

    Função csvToTable() declarada. Falta implementar.

--- libs/common.c
+++ libs/common.c
@@ -90,20 +90,25 @@ char* wordInPositionAfterSeparations(char* string, char* caracteres, int pos){
 	char m[100][100];
 	char *token;
 	token = strtok(string, caracteres);
-
+	countWords=0;
 	for(int i=0; token != NULL; i++) {
 		for(int j=0; j < (int)strlen(token); j++){
 			m[i][j] = token[j];
 		}
+		countWords++;
 		token = strtok(NULL, caracteres);
 	}
-
-	char* word=NULL;
-	for (int i = 0; i < (int)strlen(m[pos]); i++){
-		word=realloc(word, i*sizeof(char)+sizeof(char));
-		word[i]=m[pos][i];
+	if(pos+1<countWords){
+		char* word=NULL;
+		for (int i = 0; i < (int)strlen(m[pos]); i++){
+			word=realloc(word, i*sizeof(char)+sizeof(char));
+			word[i]=m[pos][i];
+		}
+		return word;
+	}else{
+		char* emptyStr="";
+		return emptyStr;
 	}
-	return word;
 }
 
 
