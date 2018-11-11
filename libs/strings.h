683644e0e33517fbbb3c2b2237d2ec12c0b6c85f
Derick Medeiros do Nascimento <nathanael.derick@gmail.com>
2018-11-11 17:16:17 -0300

    Adição de realloc na função getSubstringAfterSubstringInString()

--- libs/strings.h
+++ libs/strings.h
@@ -12,7 +12,6 @@
 *	
 */
 char* wordInPositionAfterSeparations(char* string, char* caracteres, int pos);
-
 int numberOfWords(char* string, char* caracteres);
 char* capitalize(char* string);
 char* lowerCase(char* string);
